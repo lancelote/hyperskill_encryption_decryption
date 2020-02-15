eda414d2cd9e34788da23f032c91eed3f10f91ef
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-15 11:10:08 +0300

    Stage 2 Homework 2

---
+++ src/stage2/hw1/hw2/Main.java
@@ -0,0 +1,17 @@
+package stage2.hw1.hw2;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        int offset = scanner.nextInt();
+
+        if (offset < word.length()) {
+            System.out.println(word.substring(offset) + word.substring(0, offset));
+        } else {
+            System.out.println(word);
+        }
+    }
+}
