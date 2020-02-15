bdb03cc0d2fc6b2762ae3e8d51d593aac35514a1
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-15 17:52:39 +0300

    Stage 2 Homework 9

---
+++ src/stage2/hw9/Main.java
@@ -0,0 +1,11 @@
+package stage2.hw9;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(line.toLowerCase().indexOf("the"));
+    }
+}
