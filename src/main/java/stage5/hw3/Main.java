2bb935f877965a20716da1ca73069a539cb041fc
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-25 12:34:55 +0300

    Switch to resources

--- src/main/java/stage5/hw3/Main.java
+++ src/main/java/stage5/hw3/Main.java
@@ -2,22 +2,30 @@ package stage5.hw3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "./src/main/java/stage5/hw3/dataset_91033.txt";
-        File file = new File(filePath);
-        int result = 0;
+        String fileName = "dataset_91033.txt";
+        URL resource = Main.class.getClassLoader().getResource(fileName);
 
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNext()) {
-                result += scanner.nextInt();
+        if (resource == null) {
+            System.err.println(fileName + " was not found");
+            System.exit(1);
+        } else {
+            File file = new File(resource.getFile());
+            int result = 0;
+
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNext()) {
+                    result += scanner.nextInt();
+                }
+            } catch (FileNotFoundException e) {
+                System.err.println("no such file " + fileName);
             }
-        } catch (FileNotFoundException e) {
-            System.err.println("no such file " + filePath);
-        }
 
-        System.out.println(result);
+            System.out.println(result);
+        }
     }
 }
