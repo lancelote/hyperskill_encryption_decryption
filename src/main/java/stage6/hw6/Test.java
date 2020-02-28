3975a31652dcbb55674c34fe20ac2844087d6f96
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-28 21:15:30 +0300

    Stage 6 Homework 6

---
+++ src/main/java/stage6/hw6/Test.java
@@ -0,0 +1,36 @@
+package stage6.hw6;
+
+public class Test {
+    public static void main(String[] args) {
+        new Programmer(1);
+    }
+
+    public static class TeamLead {
+        private int numTeamLead;
+
+        public TeamLead(int numTeamLead) {
+            this.numTeamLead = numTeamLead;
+            employ();
+        }
+
+        private void employ() {
+            System.out.println(numTeamLead + " teamlead");
+        }
+
+    }
+
+    public static class Programmer extends TeamLead {
+        private int numProgrammer;
+
+        public Programmer(int numProgrammer) {
+            super(numProgrammer);
+            this.numProgrammer = numProgrammer;
+            employ();
+
+        }
+
+        private void employ() {
+            System.out.println(numProgrammer + " programmer");
+        }
+    }
+}
