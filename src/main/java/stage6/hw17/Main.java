5973bf9003c4e212e7898599659c410a54e44c34
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-03-01 15:58:15 +0300

    Stage 6 Homework 17

---
+++ src/main/java/stage6/hw17/Main.java
@@ -0,0 +1,91 @@
+package stage6.hw17;
+
+import java.util.Scanner;
+
+class ClockFactory {
+
+    private boolean produceToyClock;
+
+    public ClockFactory(boolean produceToyClock) {
+        this.produceToyClock = produceToyClock;
+    }
+
+    /**
+     * It produces a clock according to a specified type: SAND, DIGITAL or MECH.
+     * If some other type is passed, the method produces ToyClock.
+     */
+    public Clock produce(String type) {
+        Clock clock = null;
+
+        if (this.produceToyClock) {
+            clock = new ToyClock();
+        }
+
+        switch (type) {
+            case "SAND":
+                clock = new SandClock();
+                break;
+            case "DIGITAL":
+                clock = new DigitalClock();
+                break;
+            case "MECH":
+                clock = new MechanicalClock();
+                break;
+        }
+
+        return clock;
+    }
+}
+
+/* Do not change code below */
+interface Clock {
+
+    void tick();
+}
+
+class SandClock implements Clock {
+
+    @Override
+    public void tick() {
+        System.out.println("...sand noise...");
+    }
+}
+
+class DigitalClock implements Clock {
+
+    @Override
+    public void tick() {
+        System.out.println("...pim...");
+    }
+}
+
+class MechanicalClock implements Clock {
+
+    @Override
+    public void tick() {
+        System.out.println("...clang mechanism...");
+    }
+}
+
+class ToyClock implements Clock {
+
+    @Override
+    public void tick() {
+        System.out.println("...tick...");
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        final Scanner scanner = new Scanner(System.in);
+        final String type = scanner.next();
+        final boolean produceToy = scanner.nextBoolean();
+        final ClockFactory factory = new ClockFactory(produceToy);
+        final Clock clock = factory.produce(type);
+        if (clock != null) {
+            clock.tick();
+        } else {
+            System.out.println(clock);
+        }
+    }
+}
