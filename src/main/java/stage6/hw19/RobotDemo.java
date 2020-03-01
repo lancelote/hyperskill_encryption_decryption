79be13960be39bb2983ff2d1487145965c3394c9
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-03-01 18:33:37 +0300

    Stage 6 Homework 19

---
+++ src/main/java/stage6/hw19/RobotDemo.java
@@ -0,0 +1,96 @@
+package stage6.hw19;
+
+import java.util.Scanner;
+
+/** Product */
+abstract class Robot {
+
+    public abstract String getName();
+    public abstract String getDescription();
+    public abstract int getPower();
+
+    @Override
+    public String toString() {
+        return "robot: {\n\t" +
+                "name : " + getName() + "\n\t" +
+                "description : " + getDescription() + "\n\t" +
+                "power : " + getPower() + "\n}";
+    }
+}
+
+/** Type of product - Robot Type */
+enum RobotType {
+    ROBOT_CLEANER
+}
+
+/** Concrete Product - Robot Cleaner */
+class RobotCleaner extends Robot {
+
+    private String name;
+    private String description;
+    private int power;
+
+    public RobotCleaner(String name, String description, int power) {
+        this.name = name;
+        this.description = description;
+        this.power = power;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public int getPower() {
+        return this.power;
+    }
+
+    @Override
+    public String toString() {
+        return "cleaner-" + super.toString();
+    }
+
+}
+
+/** Factory  */
+class RobotFactory {
+
+    /** Factory Method **/
+    public Robot getRobot(RobotType type,String name, String description,int power) {
+        Robot robot = null;
+
+        if (type == RobotType.ROBOT_CLEANER) {
+            robot = new RobotCleaner(name, description, power);
+        }
+
+        return robot;
+    }
+}
+
+/** Factory Demo */
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        RobotFactory robotFactory = new RobotFactory();
+        Scanner scanner = new Scanner(System.in);
+
+        String nameCleaner = scanner.nextLine();
+
+        Robot robotCleaner = robotFactory.
+                getRobot(
+                        RobotType.ROBOT_CLEANER,
+                        nameCleaner,
+                        "Robot will clean my room and dry my socks",
+                        100);
+
+        System.out.println(robotCleaner);
+
+        scanner.close();
+    }
+}
